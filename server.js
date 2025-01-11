4edf8fe412271ca824223e1a8a077e6a7cf3036a
cur24001@byui.edu <cur24001@byui.edu>
2025-01-11 15:27:38 -0500

    adding code

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-  res.send("Hello");
+  res.send("August Curtis");
 });
  
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+const port = 3000;
+
+app.listen(process.env.PORT || port, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || port));
 });
