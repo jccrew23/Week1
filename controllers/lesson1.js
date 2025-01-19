ebd8d6af7ebb0665d75d9a84b437c44110129bf3
cur24001@byui.edu <cur24001@byui.edu>
2025-01-19 01:28:57 -0500

    add mongodb and contacts

--- controllers/lesson1.js
+++
@@ -1,7 +0,0 @@
-const augRoute = (req, res) => {
-    res.send("August Curtis");
-    };
-
-    module.exports = {
-        augRoute,
-    };
--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
-const app = express();
-const lesson1Controller = require('./controllers/lesson1');
- 
-app.get('/', lesson1Controller.augRoute);
- 
-const port = 3000;
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb');
+const mongodb = require('./configs/db');
 
-app.listen(process.env.PORT || port, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || port));
+const port = process.env.PORT || 3000;
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      next();
+  })
+  .use('/contacts', require('./routes/contacts'));
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  }
+  else {
+    app.listen(port);
+    console.log(`Server is running on port ${port}`)
+  }
 });
